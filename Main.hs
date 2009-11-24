d728a4ff57bd13e86c4c323fedbab1629b5c9669
Lars Petersen <info@lars-petersen.net>
2009-11-24 14:30:56 +0100

    Umstellung des Userinterfaces auf Haskeline.

--- Main.hs
+++ Main.hs
@@ -1,8 +1,7 @@
 module Main where
 
-import System.Exit
-import System.Console.Readline
-import Codec.Binary.UTF8.String 
+import Data.List
+import System.Console.Haskeline
 
 import Datatypes
 import Parser
@@ -12,37 +11,32 @@ import Manual
 import SequenceCalculus
 import Prover
 
---einige String-Formatierungsfunktionen
-putUtf8Ln   = putStrLn . encodeString
-underline x = x ++ '\n':( map (const '-') x)
+main :: IO ()
+main = runInputT defaultSettings (outputStrLn manual >> loop [])
+        
+loop   :: Assumptions -> InputT IO ()
+loop as = do
+            minput <- getInputLine ">> "
+            case minput of
+              Nothing          -> return ()
+              Just "quit"      -> return ()
+              Just "exit"      -> return ()
+              Just "clear"     -> loop [] 
+              Just "ls"        -> mapM_ (outputStrLn.show) as >> loop as
+              Just "demo"      -> mapM_ (outputStrLn.(uncurry prove')) examples >> loop as
+              Just "help"      -> outputStrLn manual  >> loop as
+              Just "license"   -> outputStrLn license >> loop as
+              Just ('A':' ':l) -> case parse l of
+                                    Left err -> outputStrLn "ParseError"  >> loop as 
+                                    Right p  -> loop (as `union` [A p])
+              Just l           -> case parse l of
+                                    Left err -> outputStrLn "ParseError"  >> loop as 
+                                    Right p  -> outputStrLn (prove' p as) >> loop as
 
--- Userinterface und Hauptschleife
-main = (putUtf8Ln manual) >> readEvalPrintLoop []
-
-readEvalPrintLoop :: [Proof] -> IO ()
-readEvalPrintLoop ps = do
-  maybeLine <- readline ">> "
-  case maybeLine of 
-       Nothing     -> exitWith (ExitSuccess) -- EOF / control-d
-       Just "exit" -> exitWith (ExitSuccess) 
-       Just "clear"-> (addHistory "clear")>> readEvalPrintLoop []
-       Just "ls"   -> (addHistory "ls")   >> (putStr $ unlines $ map (encodeString.show) $ map fromProof ps) >> (readEvalPrintLoop ps)
-       Just "demo" -> (addHistory "demo") >> (sequence $ map (uncurry prove'n'print) examples) >> (readEvalPrintLoop ps)
-       Just "help" -> (addHistory "help") >> (putUtf8Ln manual) >> (readEvalPrintLoop ps)
-       Just "license" -> (addHistory "license") >> (putUtf8Ln license) >> (readEvalPrintLoop ps)
-       Just line -> do addHistory line
-                       case line of
-                        'A':' ':line' -> case parse (decodeString line') of
-                                           Left err -> do{ putStr "parse error at "; print err}
-                                           Right x  -> readEvalPrintLoop (ps `union` [A x])
-                        _             -> case parse (decodeString line) of
-                                           Left err -> do{ putStr "parse error at "; print err}
-                                           Right x  -> catch (prove'n'print x ps) (const $ readEvalPrintLoop ps) 
-                       readEvalPrintLoop ps
-
-prove'n'print     :: Proposition -> Assumptions -> IO ()
-prove'n'print p as = do putUtf8Ln $ underline ("Beweis von "++(init $ tail $ show (map fromProof as))++" ⊦ "++(show p)++":")
-                        case prove p as of
-                          Nothing -> putUtf8Ln "Nicht beweisbar."
-                          Just q  -> putUtf8Ln $ unlines $ map show $ proofToSequence q
-
+prove'     :: Proposition -> Assumptions -> String 
+prove' p as = underline ("Beweis von "++(init $ tail $ show (map fromProof as))++" ⊦ "++(show p)++":")
+              ++ "\n"
+              ++ case prove p as of
+                   Nothing -> "Nicht beweisbar."
+                   Just q  -> unlines $ map show $ proofToSequence q
+              where underline x = x ++ '\n':(map (const '-') x)
